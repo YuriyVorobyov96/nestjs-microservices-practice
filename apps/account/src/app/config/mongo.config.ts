5becbd6803ce1f5ec8cdb5d3b7cfec8041711140
YuriyVorobyov96 <darthrevan1333@gmail.com>
2023-10-25 21:06:47 +0300

    added mongo

---
+++ apps/account/src/app/config/mongo.config.ts
@@ -0,0 +1,26 @@
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModuleAsyncOptions } from "@nestjs/mongoose";
+
+export const getMongoConfig = (): MongooseModuleAsyncOptions => {
+  return {
+    useFactory: (configService :ConfigService) => ({
+      uri: getMongUri(configService),
+    }),
+    inject: [ConfigService],
+    imports: [ConfigModule],
+  }
+}
+
+const getMongUri = (configService: ConfigService) =>
+	'mongodb://' +
+	configService.get('MONGO_LOGIN') +
+	':' +
+	configService.get('MONGO_PASSWORD') +
+	'@' +
+	configService.get('MONGO_HOST') +
+	':' +
+	configService.get('MONGO_PORT') +
+	'/' +
+	configService.get('MONGO_DATABASE') +
+	'?authSource=' +
+	configService.get('MONGO_AUTHDATABASE');
