c15824f3a91b65c53ed437bcd03bcf49e5c80b53
YuriyVorobyov96 <darthrevan1333@gmail.com>
2023-10-26 10:49:01 +0300

    added auth

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,6 +1,17 @@
 import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
+export class RegisterDto {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class LoginDto {
+  email: string;
+  password: string;
+}
+
 @Controller('auth')
 export class AuthController {
   constructor(
@@ -16,15 +27,4 @@ export class AuthController {
   async login(dto: LoginDto) {
     return this.authService.login(dto);
   }
-}
-
-export class RegisterDto {
-  name: string;
-  email: string;
-  password: string;
-}
-
-export class LoginDto {
-  email: string;
-  password: string;
 }
