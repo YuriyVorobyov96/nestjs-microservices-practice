d94c447a6f1f7adbbb093be20f2cfc089c24ea8e
YuriyVorobyov96 <darthrevan1333@gmail.com>
2023-11-04 16:20:43 +0300

    updated account auth http controller to rmq controller

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,19 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AccountLogin, AccountRegister } from '@policy/contracts';
+import { RMQRoute } from 'nestjs-rmq';
 
-@Controller('auth')
+@Controller()
 export class AuthController {
   constructor(
     private readonly authService: AuthService
   ) {}
 
-  @Post('register')
+  @RMQRoute(AccountRegister.topic)
   async register(dto: AccountRegister.Request): Promise<AccountRegister.Response> {
     return this.authService.register(dto);
   }
 
-  @Post('login')
+  @RMQRoute(AccountLogin.topic)
   async login(dto: AccountLogin.Request): Promise<AccountLogin.Response> {
     return this.authService.login(dto);
   }
